136efe740d6a18554024dda95fb7a74b0cda4a02
Heggy Castaneda <heggyy@gmail.com>
2021-07-22 21:00:10 -0400

    update

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const {PORT} = process.env;
 
+app.use(express.static(path.resolve(__dirname + '/react-ui/build')));
 app.get("/heartbeat", (req, res) => {
   res.json({
     is: "working",
