fee6fe4b8039392fcf352541f4545bed72f95139
Heggy Castaneda <heggyy@gmail.com>
2021-07-22 20:15:21 -0400

    update PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const {PORT} = process.env;
 
 app.get("/heartbeat", (req, res) => {
   res.json({
