3ea01c462b3dc17eec48217aa8d87132ba3b9d2b
Heggy Castaneda <heggyy@gmail.com>
2021-07-24 09:17:50 -0400

    refactor routing out of main to its own routes dir

--- react-ui/src/sectioning/footer/footer.jsx
+++ react-ui/src/sectioning/footer/footer.jsx
@@ -1,7 +1,25 @@
+import { NavLink } from "react-router-dom";
+
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <ul>
+        <li>Copyright 2021</li>
+        <li>
+          <a
+            href="https://github.com/heggy231"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <NavLink to="/credits">Credits</NavLink>
+        </li>
+      </ul>
+    </footer>
   );
-}
+};
 
 export default Footer;
