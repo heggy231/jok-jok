716095974fd2b8cb56174d329ca518e909122484
Heggy Castaneda <heggyy@gmail.com>
2021-07-24 10:54:38 -0400

    finish store.js redux template set up

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./app";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
