2c7a9e09ea3b759c0a43e5ee909e2b95a7a09383
Heggy Castaneda <heggyy@gmail.com>
2021-07-31 20:26:55 -0400

    crashing

--- react-ui/src/routes/decide/decide.jsx
+++ react-ui/src/routes/decide/decide.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './decide.css';
 import PotentialCandidates from '../../components/PotentialCandidates';
+import data from '../../data';
+import { connect } from 'react-redux';
 
-const Decide = () => {
+const Decide = ({ gender }) => {
+
+  const [selection, setSelection] = useState([]); 
+
+  // filter m/f
+  const result = () => {
+    switch (gender) {
+      case 'female':
+        const resultMale = data.filter( maleData => maleData.gender === 'male' )
+        return resultMale;
+      case 'male':
+        const resultFemale = data.filter( femaleData => femaleData.gender === 'female' )
+        return resultFemale;
+      default: 
+        return [];
+    }
+  }
+
+  setSelection(result);
+
   return (
     <>
       <h1>Decide Page</h1>
-      <PotentialCandidates />
+      {/* filter to show m/f, mp the data */}
+      {selection.map( eachperson => {
+        return <div>{eachperson.name}</div>
+      })}
       <div className="makemeflex">
         <img src="/female.jpeg" alt="female" />
         <button className="btn btn-primary">female</button>
@@ -56,4 +80,16 @@ const Decide = () => {
   );
 };
 
-export default Decide;
+
+const mapStateToProps = (state) => {
+  // Translate Redux state into React props using { connect }
+
+  return {
+    // key is the prop that React will see, value is for Redux state.  End goal is to provide `state.count` to the `Output` Component (dumb React component).
+    // propName seen by React : value-in-state by Redux
+    gender: state.gender
+  }
+}
+
+
+export default connect(mapStateToProps)(Decide);
