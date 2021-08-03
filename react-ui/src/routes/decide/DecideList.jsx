4b6c12b44084f1f3c5f491c9bbb0d23233c76678
Heggy Castaneda <heggyy@gmail.com>
2021-08-02 20:33:17 -0400

    rating

--- react-ui/src/routes/decide/DecideList.jsx
+++ react-ui/src/routes/decide/DecideList.jsx
@@ -5,8 +5,18 @@ const DecideList = (props) => {
   const candidates = props.candidates;
   const candidateItems = candidates.map((candidate, index) => {
     return (
-      <div>
+      <div className="makemeflex-col give-margin-right-8">
         <img src={candidate.avatar} alt="person" />
+        <span>
+          <div className="rating">
+            <div className="like grow">
+              <i className="fa fa-thumbs-up fa-3x like" aria-hidden="true"></i>
+            </div>
+            <div className="dislike grow">
+              <i className="fa fa-thumbs-down fa-3x like" aria-hidden="true"></i>
+            </div>
+          </div>
+        </span>
       </div>
     );
   });
--- react-ui/src/routes/gender/Gender.jsx
+++ react-ui/src/routes/gender/Gender.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import './Gender.css';
-import { updateGender } from '../../redux/actions';
-import { connect } from 'react-redux';
+import React from "react";
+import "./Gender.css";
+import { updateGender } from "../../redux/actions";
+import { connect } from "react-redux";
 
 const Gender = ({ updateGender }) => {
-
   const handleClick = (e) => {
     e.preventDefault();
     updateGender(e.target.name);
-    console.log('e.target.name ===>', e.target.name)
-  }
+    console.log("e.target.name ===>", e.target.name);
+  };
 
   return (
     <div>
       <h1>Gender Preference</h1>
       <p>Do you like to date Male or Female?</p>
-      <img src="/male.jpeg" alt="male" />
-      <button onClick={handleClick} name="male" className="btn btn-primary">male</button>
-      <img src="/female.jpeg" alt="female" />
-      <button onClick={handleClick} name="female" className="btn btn-primary">female</button>
+      <div className="makemeflex">
+        <div className="makemeflex-col give-margin-right-8">
+          <img src="/male.jpeg" alt="male" className="give-margin-bottom-2" />
+          <button onClick={handleClick} name="male" className="btn btn-primary">
+            male
+          </button>
+        </div>
+        <div className="makemeflex-col">
+          <img src="/female.jpeg" alt="female" className="give-margin-bottom-2"/>
+          <button
+            onClick={handleClick}
+            name="female"
+            className="btn btn-primary"
+          >
+            female
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
